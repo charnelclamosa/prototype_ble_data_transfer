d13f97a188f6eb7b1a44b44cc1b572c650f6826e
Charnel Clamosa <charnel.clamosa16@gmail.com>
2024-04-15 20:43:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/poc_bluetooth_transfer/MainActivity.kt
@@ -0,0 +1,130 @@
+package com.example.poc_bluetooth_transfer
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.example.poc_bluetooth_transfer.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+    var handler: Handler = Handler()
+    var runnable: Runnable? = null
+    var delay = 1000
+
+    private val bluetoothManager by lazy {
+        applicationContext.getSystemService(BluetoothManager::class.java)
+    }
+
+    private val bluetoohAdapter by lazy {
+        bluetoothManager?.adapter
+    }
+    private val isBluetoothEnabled: Boolean
+        get() = bluetoohAdapter?.isEnabled == true
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        enableEdgeToEdge();
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root);
+
+        val bluetoothManager = applicationContext.getSystemService(BLUETOOTH_SERVICE) as BluetoothManager
+        val bluetoothAdapter = bluetoothManager.adapter
+
+        val enableBluetoothLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) {
+            /* handle result */
+        }
+
+        val permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { perms ->
+            val canEnabledBluetooth = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                perms[Manifest.permission.BLUETOOTH_CONNECT] == true
+            } else true
+
+            if (canEnabledBluetooth && !isBluetoothEnabled) {
+                enableBluetoothLauncher.launch(
+                    Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                )
+            }
+        }
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            permissionLauncher.launch(
+                arrayOf(
+                    Manifest.permission.BLUETOOTH_CONNECT,
+                    Manifest.permission.BLUETOOTH_SCAN
+                )
+            )
+        }
+
+        binding.btnBluetoothDiscoverable.setOnClickListener {
+            if (!bluetoothAdapter.isDiscovering) {
+                Toast.makeText(this, "Making device discoverable...", Toast.LENGTH_SHORT).show()
+                var  intent = Intent(Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE))
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.BLUETOOTH_ADVERTISE
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return@setOnClickListener
+                }
+                startActivityForResult(intent, 1)
+            }
+        }
+    }
+
+    // This function will run every second to update the
+    // Bluetooth status text bar and image.
+    override fun onResume() {
+        handler.postDelayed(Runnable {
+            handler.postDelayed(runnable!!, delay.toLong())
+            updateBluetoothStatus()
+        }.also { runnable = it }, delay.toLong())
+        super.onResume()
+    }
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(runnable!!)
+    }
+
+    private fun bluetoothIndicator(): Boolean {
+        val bluetoothManager = applicationContext.getSystemService(BLUETOOTH_SERVICE) as BluetoothManager
+        val bluetoothAdapter = bluetoothManager.adapter
+        return bluetoothAdapter.isEnabled
+    }
+    private fun updateBluetoothStatus() {
+        val isBluetoothEnabled = bluetoothIndicator();
+        if (isBluetoothEnabled) {
+            binding.img.setImageResource(R.drawable.ic_bluetooth_on)
+            binding.textStatus.setText("Bluetooth is enabled")
+        } else {
+            binding.img.setImageResource(R.drawable.ic_bluetooth_off)
+            binding.textStatus.setText("Bluetooth is disabled")
+        }
+    }
+
+
+
+}
